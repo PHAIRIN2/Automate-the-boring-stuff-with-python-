a6ae7b04df9a8019c11a4eb13550561cd29ca806
NAMZA <banana@MacBook-Air--NAMZA.local>
2026-07-07 08:15:01 +0700

    add

--- Code/Projects/chapter6_practice_project.py
+++ Code/Projects/chapter6_practice_project.py
@@ -1,4 +1,4 @@
-# Table Printer
+
 
 
 tableData = [
@@ -17,27 +17,7 @@ def printTable(tableData):
 
     for rowIndex in range(len(tableData[0])):
         for colIndex in range(len(tableData)):
-            print(tableData[colIndex][rowIndex].rjust(colWidths[colIndex]), end=" ")
-        print()
-
-
-tableData = [
-    ["apples", "oranges", "cherries", "banana"],
-    ["Alice", "Bob", "Carol", "David"],
-    ["dogs", "cats", "moose", "goose"],
-]
-
-
-def printTable(data):
-    colWidths = [0] * len(data)
-    for i in range(len(data)):
-        for items in data[i]:
-            if len(items) > colWidths[i]:
-                colWidths[i] = len(items)
-
-    for col in range(len(data[0])):
-        for row in range(len(data)):
-            print(data[row][col].rjust(colWidths[row]), end=" ")
+            print(tableData[colIndex][rowIndex].rjust(colWidths[colIndex]), end= " ")
         print()
 
 
